687784af6e0a6bf88d0966ad6e542370c51e0b95
yxzhk <yxzhk@hotmail.com>
2018-07-02 19:42:47 +0800

    skip md

--- Filter/rxjsSkipWhile.ts
+++ Filter/rxjsSkipWhile.ts
@@ -1,4 +1,4 @@
-import { interval, Observable } from "rxjs";
+import { interval } from "rxjs";
 import { skipWhile } from "rxjs/operators";
 
 
@@ -6,14 +6,14 @@ export class SkipWhilePoc {
 
     public test() {
         this.func1();
-        // this.func2();
     }
 
     public func1() {
         // emit every 1s
         const source = interval(1000);
         // skip emitted values from source while value is less than 5
-        const example = source.pipe(skipWhile((val) => val < 5));
+        const skipL5 = skipWhile((val) => val < 5);
+        const example = source.pipe(skipL5);
         // output: 5...6...7...8........
         const subscribe = example.subscribe((val) => console.log(val));
     }
---
+++ Filter/skipWhile.md
@@ -0,0 +1,22 @@
+# skipWhile
+
+#### signature: `skipWhile(predicate: Function): Observable`
+
+## Skip emitted values from source until provided expression is false.
+
+### Examples
+
+##### Example 1: Skip while values below threshold
+
+```js
+import { interval } from "rxjs";
+import { skipWhile } from "rxjs/operators";
+
+// emit every 1s
+const source = interval(1000);
+// skip emitted values from source while value is less than 5
+const skipL5 = skipWhile((val) => val < 5);
+const example = source.pipe(skipL5);
+// output: 5...6...7...8........
+const subscribe = example.subscribe((val) => console.log(val));
+```
